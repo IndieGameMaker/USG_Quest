ef77e040cdb349350a4c27128fb4b431abbb72a7
myleje@gmail.com <myleje@gmail.com>
2022-02-10 15:41:22 +0900

    .

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -44,10 +44,15 @@ public class LaserPointer : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
         {
             line.SetPosition(1, Vector3.forward * hit.distance);
+            laserMaker.position = hit.point;
+            // 각도를 법선벡터 방향으로 회전
+            laserMaker.rotation = Quaternion.LookRotation(hit.normal);
         }
         else
         {
             line.SetPosition(1, Vector3.forward * maxDistance);
+            laserMaker.position = transform.position + (transform.forward * maxDistance);
+            laserMaker.rotation = Quaternion.LookRotation(transform.position);
         }
     }
 }
