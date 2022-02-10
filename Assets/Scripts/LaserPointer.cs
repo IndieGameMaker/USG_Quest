5d14566d473501c50167000fe034f9b4d98fd165
myleje@gmail.com <myleje@gmail.com>
2022-02-10 15:12:32 +0900

    .

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour
+{
+    private RaycastHit hit;
+    private LineRenderer line;
+    public Color color = Color.blue;
+    public float maxDistance = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLineRenderer();
+    }
+
+    void CreateLineRenderer()
+    {
+        // LineRenderer 추가
+        line = this.gameObject.AddComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.receiveShadows = false;
+
+        // 시작, 끝지점을 설정
+        line.positionCount = 2;
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, Vector3.forward * maxDistance);
+
+        // 라인의 폭 설정
+        line.startWidth = 0.03f;
+        line.endWidth = 0.005f;
+        line.numCapVertices = 10;
+
+        // 라인의 머티리얼 지정, 색상을 설정
+        line.material = new Material(Shader.Find("Unlit/Color"));
+        line.material.color = this.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
